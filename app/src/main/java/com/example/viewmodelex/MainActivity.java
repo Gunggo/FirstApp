7a994d77933b39c63e28b0a1b9a26b6f9d2e184c
BangDDong <gusehdqkd@naver.com>
2019-10-22 23:36:04 +0900

    최초등록

---
+++ app/src/main/java/com/example/viewmodelex/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.viewmodelex;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private WorkoutsFragment workoutsFragment = new WorkoutsFragment();
+    private SettingFragment settingFragment = new SettingFragment();
+    private LogsFragment logsFragment = new LogsFragment();
+    private ExeploreFragment exeploreFragment = new ExeploreFragment();
+    private ExerciesesFragment exerciesesFragment = new ExerciesesFragment();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, workoutsFragment).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                FragmentTransaction transaction1 = fragmentManager.beginTransaction();
+
+                switch (menuItem.getItemId()) {
+                    case R.id.workouts:
+                        transaction1.replace(R.id.frameLayout, workoutsFragment).commitAllowingStateLoss();
+                        break;
+                    case R.id.exercises:
+                        transaction1.replace(R.id.frameLayout, exerciesesFragment).commitAllowingStateLoss();
+                        break;
+                    case R.id.logs:
+                        transaction1.replace(R.id.frameLayout, logsFragment).commitAllowingStateLoss();
+                        break;
+                    case R.id.explore:
+                        transaction1.replace(R.id.frameLayout, exeploreFragment).commitAllowingStateLoss();
+                        break;
+                    case R.id.setting:
+                        transaction1.replace(R.id.frameLayout, settingFragment).commitAllowingStateLoss();
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+}
